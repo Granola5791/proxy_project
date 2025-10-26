3dd53511a51d2c90e8ca7b00fef969d1b924b6e3
Granola5791 <yuvalby1969@gmail.com>
2025-10-26 19:39:46 +0200

    extracted handle client code to a dedicated function.

--- proxy_computer/main.go
+++ proxy_computer/main.go
@@ -8,9 +8,17 @@ import (
 	"github.com/google/gopacket/pcap"
 )
 
-func main() {
-	// Open the network device
-	handle, err := pcap.OpenLive("eth0", 1600, false, pcap.BlockForever)
+func HandleClientConnection() {
+	handle, err := pcap.OpenLive(
+		GetStringFromConfig("network_device"),
+		int32(GetIntFromConfig("pcap_snaplen")),
+		false,
+		pcap.BlockForever,
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = handle.SetBPFFilter("port 8080")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -22,6 +30,12 @@ func main() {
 	// Listen for packets
 	fmt.Println("Listening for packets...")
 	for packet := range packets.Packets() {
-		fmt.Println("Received packet:", packet)
+		fmt.Println("Received packet:", packet.NetworkLayer())
 	}
+}
+
+func main() {
+	InitConfig()
+	go HandleClientConnection()
+	select {}
 }
