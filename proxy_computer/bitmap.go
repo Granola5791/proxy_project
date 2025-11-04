5503751efb0618727eaafeda0272ddd3c64fdc45
Granola5791 <yuvalby1969@gmail.com>
2025-11-04 18:10:31 +0200

    created bitmap type.

---
+++ proxy_computer/bitmap.go
@@ -0,0 +1,25 @@
+package main
+
+type BitMap struct{
+	Bitmap uint64
+}
+
+func NewBitMap() *BitMap {
+	return &BitMap{Bitmap: 0}
+}
+
+func (b *BitMap) SetBitOn(index int) {
+	b.Bitmap |= 1 << index
+}
+
+func (b *BitMap) SetBitOff(index int) {
+	b.Bitmap &= ^(1 << index)
+}
+
+func (b *BitMap) GetBit(index int) bool {
+	return (b.Bitmap & (1 << index)) != 0
+}
+
+func (b *BitMap) ToggleBit(index int) {
+	b.Bitmap ^= 1 << index
+}
